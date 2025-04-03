d0e4d9149948e5a9f4bf55aff17ee3a00d26e601
Eduar_Sg <eduardosegura100204@gmail.com>
2025-04-03 13:10:49 -0400

    Fixing components dimensions

--- frontend/src/layouts/default.tsx
+++ frontend/src/layouts/default.tsx
@@ -4,7 +4,7 @@ export default function CreatorLayout({ children }: { children: React.ReactNode 
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <div className="flex flex-col flex-grow">
+      <div className="flex flex-col flex-grow ml-80">
         <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16 bg-rojo-intec-50">
           {children}
         </main>
